39d2aa68acb2a96c2ceef1d9079a2947ba1ed6d7
Filipe Dias <filipediascontato@gmail.com>
2020-06-03 17:59:56 -0300

    Implements item listing

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { response } from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
@@ -6,5 +7,17 @@ const routes = express.Router();
 routes.get('/', (request, response) => {
     return response.json({ message: 'Hello World' });
 });
+
+routes.get('/items', async (request, response) => {
+    const items =  await knex('items').select('*');
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+    });
+
+    return response.json(serializedItems);
+})
 
 export default routes;
