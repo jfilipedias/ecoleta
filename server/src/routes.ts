c9e057dc14cb9c89f36ebca3b8effa87b8386856
Filipe Dias <filipediascontato@gmail.com>
2020-06-04 11:20:16 -0300

    Implements specific point listing and filtered points listing

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,8 +7,10 @@ const routes = express.Router();
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
-routes.get('/items', );
+routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
+routes.get ('/points', pointsController.index);
+routes.get ('/points/:id', pointsController.show);
 
 export default routes;
