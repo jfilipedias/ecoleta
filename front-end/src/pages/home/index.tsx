2663ceb704610778175a126ecb3ab11f93296cf4
Filipe Dias <filipediascontato@gmail.com>
2020-06-05 11:00:40 -0300

    Implement the routes

--- front-end/src/pages/home/index.tsx
+++ front-end/src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -16,12 +17,12 @@ const Home = () => {
           <h1>Seu marketplace de coleta de lixo</h1>
           <p>Ajudamos pessoas a encontrar pontos de coleta de forma eficiente</p>
 
-          <a href="">
+          <Link to="/create-point">
             <span>
               <FiLogIn />
             </span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
