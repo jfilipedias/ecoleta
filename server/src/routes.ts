80420b3bb8ce38aaad62bd0ff7a55f154c3b2323
Filipe Dias <filipediascontato@gmail.com>
2020-06-08 12:04:45 -0300

    Implement data validation

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -19,8 +19,17 @@ routes.post('/points',
   upload.single('image'),
   celebrate({
     body: Joi.object().keys({
-      
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
     }),
+  }, {
+    abortEarly: false
   }),
   pointsController.create);
 
